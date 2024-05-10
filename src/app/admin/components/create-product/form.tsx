35f01066f686f987d329b1906381fd6041a877b4
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-05-10 13:56:32 +0200

    chore: Update CreateProductForm component layout and styles

--- src/app/admin/components/create-product/form.tsx
+++ src/app/admin/components/create-product/form.tsx
@@ -1,13 +1,23 @@
 export const CreateProductForm = () => {
   return (
-    <div>
-      <form className='flex flex-col gap-5 justify-center items-center border border-solid border-gray-200'>
-        <label htmlFor='name'>Name</label>
-        <input id='name' name='name' type='text' />
-        <label htmlFor='price'>Price</label>
-        <input id='price' name='price' type='text' />
-        <label htmlFor='description'>Description</label>
-        <input id='description' name='description' type='text' />
+    <div className='flex flex-col items-center'>
+      <form className='flex flex-col gap-5 justify-center items-center border border-solid border-gray-200 shadow-lg md:w-1/2'>
+        <input type='text' placeholder='Nombre del producto' className='p-2 border border-solid border-gray-200 rounded-md shadow-lg' />
+        <div className='flex gap-5'>
+          <input type='number' placeholder='Precio del producto' />
+          <input type='number' placeholder='Precio con descuento' />
+        </div>
+        <div className='flex gap-5'>
+          <input type='text' placeholder='Referencia' />
+          <input type='number' placeholder='Stock' />
+        </div>
+        <div>
+          <label htmlFor='file'>
+            Imagen
+            <input type='file' id='file' className='hidden' accept='image/*' />
+          </label>
+        </div>
+        <textarea rows={10} placeholder='Descripción del producto' />
         <button type='submit'>Submit</button>
       </form>
     </div>
