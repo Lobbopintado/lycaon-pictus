4f39f93f93197572602bb8aaf45c4290326f48f4
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-05-12 16:14:09 +0200

    chore: Update WhatsAppButton component with new phone number format

--- src/components/whatsapp-button.tsx
+++ src/components/whatsapp-button.tsx
@@ -1,7 +1,7 @@
 export const WhatsappButton = () => {
   return (
     <a
-      href='https://wa.me/573123456789'
+      href='https://wa.me/+34603255785'
       target='_blank'
       className='fixed bottom-4 right-4 rounded-full shadow-md' rel='noreferrer'
     >
