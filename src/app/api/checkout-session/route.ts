440f909bbb36f263ebf586bc630803ab4c7616b0
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-05-29 13:59:17 +0200

    chore: Remove unused CORS middleware and update npm dependencies

--- src/app/api/checkout-session/route.ts
+++ src/app/api/checkout-session/route.ts
@@ -1,5 +1,4 @@
 /* eslint-disable camelcase */
-import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
@@ -9,7 +8,6 @@ export async function POST (request: Request) {
   headers.set('Access-Control-Allow-Origin', 'http://localhost:3000')
   headers.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE')
   headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  headers.set('cors', 'no-cors')
   const lineItems = []
   for (const item of products) {
     lineItems.push({
@@ -25,12 +23,12 @@ export async function POST (request: Request) {
   }
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
     line_items: lineItems,
     mode: 'payment',
     success_url: `${request.headers.get('origin')}/success`,
     cancel_url: `${request.headers.get('origin')}/cancel`
   })
   const redirectUrl = session.url || ''
-  return NextResponse.redirect(redirectUrl, { status: 303 })
+  console.log(redirectUrl)
+  return new Response(JSON.stringify({ url: redirectUrl }), { headers })
 }
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { CardCart } from './components/card-cart'
 import { useGetProductsOfCart } from './hooks/use-get-products-of-cart'
@@ -9,7 +10,7 @@ export default function Cart () {
   const [total, setTotal] = useState(0)
   const [ivaOfTotal, setIvaOfTotal] = useState(0)
   const [allProducts, setAllProducts] = useState([])
-  console.log(allProducts)
+  const router = useRouter()
 
   useEffect(() => {
     setIvaOfTotal(total - (total / 1.21))
@@ -31,14 +32,51 @@ export default function Cart () {
       },
       body: JSON.stringify({ products: allProducts })
     })
-    console.log(response)
+    const { url } = await response.json()
+    if (url) {
+      localStorage.setItem('buy', JSON.stringify(allProducts))
+      router.push(url)
+    }
   }
 
   return (
-    <section className='bg-white py-8 antialiased md:py-16'>
-      <div className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
-        <h2 className='text-xl font-semibold text-gray-900 sm:text-2xl'>Carrito</h2>
-        <div className='mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8'>
+    <section className='py-8 md:py-16 flex flex-col md:flex-row px-10 w-screen gap-10 justify-start items-start'>
+      <form className='flex flex-col gap-5 p-5 justify-center items-center border md:border-solid border-gray-200 md:shadow-lg md:w-1/2 w-full border-none rounded-md'>
+        <label htmlFor='name' className='w-full text-lg font-bold'>
+          Nombre Completo
+          <input type='text' id='name' name='name' className='p-2 border border-solid border-gray-200 rounded-md shadow-lg w-full' />
+        </label>
+        <label htmlFor='email' className='w-full text-lg font-bold'>
+          Email
+          <input type='email' id='email' name='email' className='p-2 border border-solid border-gray-200 rounded-md shadow-lg w-full' />
+        </label>
+        <div className='flex gap-5 w-full flex-col md:flex-row'>
+          <label htmlFor='reference' className='w-full text-lg font-bold'>
+            Dirección
+            <input type='text' name='address' className='p-2 border border-solid border-gray-200 rounded-md shadow-lg w-full' />
+          </label>
+          <label htmlFor='stock' className='w-full text-lg font-bold'>
+            Código Postal
+            <input type='number' name='stock' className='p-2 border border-solid border-gray-200 rounded-md shadow-lg w-full' />
+          </label>
+        </div>
+        <div className='flex gap-5 w-full flex-col md:flex-row'>
+          <label htmlFor='reference' className='w-full text-lg font-bold'>
+            Población
+            <input type='text' name='reference' className='p-2 border border-solid border-gray-200 rounded-md shadow-lg w-full' />
+          </label>
+          <label htmlFor='stock' className='w-full text-lg font-bold'>
+            Provincia
+            <input type='text' name='city' className='p-2 border border-solid border-gray-200 rounded-md shadow-lg w-full' />
+          </label>
+        </div>
+        <label htmlFor='ml' className='w-full text-lg font-bold'>
+          Teléfono
+          <input type='number' name='ml' className='p-2 border border-solid border-gray-200 rounded-md shadow-lg w-full' />
+        </label>
+      </form>
+      <div className='w-full md:w-1/2'>
+        <div className='md:gap-6 lg:items-start xl:gap-8 flex flex-col'>
           <div className='mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl'>
             <section className='space-y-6'>
               {
@@ -48,10 +86,9 @@ export default function Cart () {
               }
             </section>
           </div>
-          <div className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'>
-            <div className='space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm sm:p-6'>
+          <div className='w-full mt-5 flex-1 space-y-6 lg:mt-0 lg:w-full'>
+            <div className='rounded-lg border border-gray-200 bg-white p-5 shadow-sm w-full flex flex-col gap-3'>
               <p className='text-xl font-semibold text-gray-900'>Resumen del pedido</p>
-
               <div className='space-y-4'>
                 <div className='space-y-2'>
                   <dl className='flex items-center justify-between gap-4'>
@@ -59,19 +96,16 @@ export default function Cart () {
                     </dt>
                     <dd className='text-base font-medium text-gray-900'>{(total - ivaOfTotal).toFixed(2)}€</dd>
                   </dl>
-
                   <dl className='flex items-center justify-between gap-4'>
                     <dt className='text-base font-normal text-gray-500'>IVA (21%)</dt>
                     <dd className='text-base font-medium text-gray-900'>{ivaOfTotal.toFixed(2)}€</dd>
                   </dl>
                 </div>
-
                 <dl className='flex items-center justify-between gap-4 border-t border-gray-200 pt-2'>
                   <dt className='text-base font-bold text-gray-900'>Total</dt>
                   <dd className='text-base font-bold text-gray-900'>{total.toFixed(2)}€</dd>
                 </dl>
               </div>
-
               <button onClick={handleClick} className='flex w-full items-center justify-center rounded-lg bg-green-400 px-5 py-2.5 text-sm font-medium text-black hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300'>Proceder con el pago</button>
               <div className='flex items-center justify-center gap-2'>
                 <span className='text-sm font-normal text-gray-500'> o </span>
