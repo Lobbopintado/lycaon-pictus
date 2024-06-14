e2afa80042850d035f04a272251027cbd6556728
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2024-06-14 22:32:00 +0200

    Modify feature category image

--- src/components/feature-cotegory.tsx
+++ src/components/feature-cotegory.tsx
@@ -3,21 +3,21 @@ import Link from 'next/link'
 /* eslint-disable @next/next/no-img-element */
 export const FeatureCategory = () => {
   return (
-    <ul className='w-full px-3 md:p-0 md:w-2/3 h-[500px] grid grid-cols-2 md:grid-cols-3 grid-rows-2 gap-3 place-items-start mb-14'>
-      <li>
+    <ul className='w-full justify-center gap-5 px-3 md:p-0 md:w-2/3 flex flex-wrap md:flex-nowrap mb-14'>
+      <li className='w-[45%] md:h-[30%]'>
         <Link href='/categoria/1-4-butandiol' className='flex flex-col justify-end items-end'>
           <img src='/products/butadeniol.jpeg' alt='Butadeniol' className='size-full md:size-96 object-contain' />
           <h3 className='w-full text-center p-2 text-black font-bold'>1.4 - Butadeniol (BDO)</h3>
         </Link>
       </li>
-      <li>
+      <li className='w-[45%] md:w-[30%]'>
         <Link href='/categoria/multi-gel-remover' className='flex flex-col justify-end items-end'>
           <img src='/products/remover.jpeg' alt='Butadeniol' className='size-full object-contain md:size-96' />
           <h3 className='w-full text-center p-2 text-black font-bold'>Multi remover</h3>
         </Link>
       </li>
-      <li className='flex justify-center col-span-2 md:col-span-1'>
-        <Link href='/poppers' className='flex flex-col justify-end items-end w-1/2 md:w-full'>
+      <li className='md:w-[30%] w-3/4'>
+        <Link href='/poppers' className='flex flex-col'>
           <img src='/products/poppers.jpeg' alt='Butadeniol' className='size-full object-contain md:size-96' />
           <h3 className='text-sm w-full text-center p-2 text-black font-bold'>Poppers (Pentilo, Amilo, Propilo y Mixtos)</h3>
         </Link>
